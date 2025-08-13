a6b8ea7f0bcf34250d5dde67adf732186b853eb0
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-08-13 15:20:49 -0300

    desnevolvo a pagina cadastrar no angular material

--- src/app/pages/cadastrar/cadastrar.component.ts
+++ src/app/pages/cadastrar/cadastrar.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIcon } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../footer/footer.component';
+
 
 @Component({
   selector: 'app-cadastrar',
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule, MatIcon,MatFormFieldModule,MatInputModule,MatButtonModule,MatSelectModule, FooterComponent],
   templateUrl: './cadastrar.component.html',
   styleUrl: './cadastrar.component.css'
 })
 export class CadastrarComponent {
+     
+    formCadastro!: FormGroup;
+    hide = true; // senha escondida por padrão
 
+    constructor(private fb: FormBuilder){
+      this.formCadastro = this.fb.group({
+          Email: ['', Validators.required],
+          Senha: ['', Validators.required],
+          Perfil: ['', Validators.required],
+      })
+    }
+
+     onSubmit() {
+    if (this.formCadastro.valid) {
+      console.log(this.formCadastro.value);
+      alert('Formulário enviado com sucesso!');
+    } else {
+      alert('Preencha todos os campos corretamente.');
+    }
+  }
 }
