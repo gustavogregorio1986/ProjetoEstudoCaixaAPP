109cb88e7ae00136f46ca6c8e6c7ca87a3bb04af
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-08-13 13:16:12 -0300

    desenvolo a pagina sobre com angular material

--- src/app/pages/sobre/sobre.component.ts
+++ src/app/pages/sobre/sobre.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-sobre',
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatButtonModule,MatIconModule, FooterComponent],
   templateUrl: './sobre.component.html',
   styleUrl: './sobre.component.css'
 })
 export class SobreComponent {
 
+  showMore = false; // controla se mostra o conteúdo extra
+
+  toggleShowMore() {
+    this.showMore = !this.showMore;
+  }
 }
